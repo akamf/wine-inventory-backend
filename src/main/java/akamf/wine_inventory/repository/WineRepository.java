d0ab959441690201ff7a5383df2f7bbb6fcd8684
Andreas Kamf <andreaskamf@gmail.com>
2025-03-15 16:32:57 +0100

    rm @Repository from WineRepositroy

--- src/main/java/akamf/wine_inventory/repository/WineRepository.java
+++ src/main/java/akamf/wine_inventory/repository/WineRepository.java
@@ -2,8 +2,6 @@ package akamf.wine_inventory.repository;
 
 import akamf.wine_inventory.model.Wine;
 import org.springframework.data.mongodb.repository.ReactiveMongoRepository;
-import org.springframework.stereotype.Repository;
 
-@Repository
 public interface WineRepository extends ReactiveMongoRepository<Wine, String> {
 }
