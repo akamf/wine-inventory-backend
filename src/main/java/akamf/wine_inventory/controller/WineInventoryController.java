37ef5c1299e86df5478cd53c560072e12cc3a87b
Andreas Kamf <andreaskamf@gmail.com>
2025-03-19 15:38:26 +0100

    Controllers for User and WineInventory

---
+++ src/main/java/akamf/wine_inventory/controller/WineInventoryController.java
@@ -0,0 +1,24 @@
+package akamf.wine_inventory.controller;
+
+import akamf.wine_inventory.model.WineInventory;
+import akamf.wine_inventory.service.WineInventoryService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.*;
+import reactor.core.publisher.Mono;
+
+@RestController
+@RequestMapping("/inventories")
+@RequiredArgsConstructor
+public class WineInventoryController {
+    private final WineInventoryService wineInventoryService;
+
+    @GetMapping("/{userId}")
+    public Mono<WineInventory> getInventory(@PathVariable String userId) {
+        return wineInventoryService.getInventoryByUserId(userId);
+    }
+
+    @PostMapping
+    public Mono<WineInventory> createInventory(@RequestBody WineInventory inventory) {
+        return wineInventoryService.createInventory(inventory);
+    }
+}
