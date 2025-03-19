b0b866a84d444afa56260dbd2e624f99c0877fd9
Andreas Kamf <andreaskamf@gmail.com>
2025-03-19 15:46:38 +0100

    added methods to save/remove wine from inventory

--- src/main/java/akamf/wine_inventory/service/WineInventoryService.java
+++ src/main/java/akamf/wine_inventory/service/WineInventoryService.java
@@ -1,10 +1,14 @@
 package akamf.wine_inventory.service;
 
+import akamf.wine_inventory.model.Wine;
+import akamf.wine_inventory.model.WineEntry;
 import akamf.wine_inventory.model.WineInventory;
 import akamf.wine_inventory.repository.WineInventoryRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 import reactor.core.publisher.Mono;
+
+import java.time.LocalDateTime;
 
 @Service
 @RequiredArgsConstructor
@@ -17,5 +21,29 @@ public class WineInventoryService {
 
     public Mono<WineInventory> createInventory(WineInventory inventory) {
         return wineInventoryRepository.save(inventory);
+    }
+
+    public Mono<WineInventory> addWineToInventory(String userId, Wine wine) {
+        return wineInventoryRepository.findByUserId(userId)
+            .flatMap(inventory -> {
+                String wineId = wine.id();
+                WineEntry newEntry = new WineEntry(wineId, wine, LocalDateTime.now(), null);
+                inventory.currentWines().put(wineId, newEntry);
+                return wineInventoryRepository.save(inventory);
+            });
+    }
+
+    public Mono<WineInventory> removeWineFromInventory(String userId, String wineId) {
+        return wineInventoryRepository.findByUserId(userId)
+            .flatMap(inventory -> {
+                if (inventory.currentWines().containsKey(wineId)) {
+                    WineEntry entry = inventory.currentWines().remove(wineId);
+                    WineEntry historyEntry = new WineEntry(
+                            entry.id(), entry.wine(), entry.dateAcquired(), LocalDateTime.now()
+                    );
+                    inventory.wineHistory().put(wineId, historyEntry);
+                }
+                return wineInventoryRepository.save(inventory);
+            });
     }
 }
