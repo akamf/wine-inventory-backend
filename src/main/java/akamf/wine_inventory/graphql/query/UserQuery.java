56acb88e40163cc34dff7398a304bbd3d7c4a591
Andreas Kamf <andreaskamf@gmail.com>
2025-03-24 09:09:34 +0100

    refactor UserController for graphql

---
+++ src/main/java/akamf/wine_inventory/graphql/query/UserQuery.java
@@ -0,0 +1,22 @@
+package akamf.wine_inventory.graphql.query;
+
+import akamf.wine_inventory.model.User;
+import akamf.wine_inventory.service.UserService;
+
+import com.netflix.graphql.dgs.DgsComponent;
+import com.netflix.graphql.dgs.DgsQuery;
+import com.netflix.graphql.dgs.InputArgument;
+import lombok.RequiredArgsConstructor;
+import reactor.core.publisher.Mono;
+
+@DgsComponent
+@RequiredArgsConstructor
+public class UserQuery {
+
+    private final UserService userService;
+
+    @DgsQuery
+    public Mono<User> getUser(@InputArgument String username) {
+        return userService.getUserByUsername(username);
+    }
+}
